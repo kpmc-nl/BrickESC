8d2b42a8d53e7ff67fa73f66a0660f32fbae6c5e
Matthijs Oosterhoff <matthijs.oosterhoff@gmail.com>
2016-12-04 16:29:59 +0100

    Just use arduino toolchain. (from arduino 1.6.0!)

--- cmake/build-env.example.cmake
+++ cmake/build-env.example.cmake
@@ -1,4 +1,3 @@
 set(ARDUINO_PATH /Applications/Arduino.app/Contents/Java)
 set(ATTINY_CORE_PATH /Users/me/Documents/Arduino/hardware/ATTinyCore)
 set(AVRDUDE_PROGRAMMER_FLAGS -cusbasp)
-set(USE_ARDUINO_TOOLCHAIN)
