81e5322995234e93528b81949afa4e83b568421e
Matthijs Oosterhoff <matthijs.oosterhoff@gmail.com>
2016-12-04 15:17:36 +0100

    Switch config .txt files to build-env.cmake

--- cmake/arduino-1.6.cmake
+++ cmake/arduino-1.6.cmake
@@ -1,20 +1,20 @@
-file(STRINGS "arduino-path.txt" ARDUINO_ROOT)
 
-if (NOT ARDUINO_ROOT)
+if (NOT ARDUINO_PATH)
     message(FATAL_ERROR "Specify the Arduino SDK path in the textfile arduino-path.txt")
 endif ()
 
-
 enable_language(ASM)
 
-set(ARDUINO_CORE_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/cores/arduino/")
-set(ARDUINO_PINS_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/variants/${ARDUINO_VARIANT}")
+set(ARDUINO_CORE_DIR "${ARDUINO_PATH}/hardware/arduino/avr/cores/arduino/")
+set(ARDUINO_PINS_DIR "${ARDUINO_PATH}/hardware/arduino/avr/variants/${ARDUINO_VARIANT}")
 
 if (NOT AVRDUDE_CONFIG)
-    set(AVRDUDE_CONFIG "${ARDUINO_ROOT}/hardware/tools/avr/etc/avrdude.conf")
+    set(AVRDUDE_CONFIG "${ARDUINO_PATH}/hardware/tools/avr/etc/avrdude.conf")
 endif ()
 
-set(AVR_GCC ${ARDUINO_ROOT}/hardware/tools/avr/bin/avr-gcc)
-set(AVR_G++ ${ARDUINO_ROOT}/hardware/tools/avr/bin/avr-g++)
-set(AVR_OBJCOPY ${ARDUINO_ROOT}/hardware/tools/avr/bin/avr-objcopy)
-set(AVRDUDE ${ARDUINO_ROOT}/hardware/tools/avr/bin/avrdude -C${AVRDUDE_CONFIG})
+if (USE_ARDUINO_TOOLCHAIN)
+    set(AVR_GCC ${ARDUINO_PATH}/hardware/tools/avr/bin/avr-gcc)
+    set(AVR_G++ ${ARDUINO_PATH}/hardware/tools/avr/bin/avr-g++)
+    set(AVR_OBJCOPY ${ARDUINO_PATH}/hardware/tools/avr/bin/avr-objcopy)
+    set(AVRDUDE ${ARDUINO_PATH}/hardware/tools/avr/bin/avrdude -C${AVRDUDE_CONFIG})
+endif ()
---
+++ cmake/build-env.example.cmake
@@ -0,0 +1,4 @@
+set(ARDUINO_PATH /Applications/Arduino.app/Contents/Java)
+set(ATTINY_CORE_PATH /Users/me/Documents/Arduino/hardware/ATTinyCore)
+set(AVRDUDE_PROGRAMMER_FLAGS -cusbasp)
+set(USE_ARDUINO_TOOLCHAIN)
