9e20bee66252313364c2247d126493d8dfcc6ffb
Matthijs Oosterhoff <matthijs@matthijs-mac.local>
2019-12-13 22:16:38 +0100

    Simplified input filter

--- src/util.c
+++ src/util.c
@@ -8,31 +8,3 @@ long map(long x, long in_min, long in_max, long out_min, long out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-
-
-uint64_t median(uint64_t *input, uint64_t array_size) {
-
-    uint64_t sorted[array_size];
-    for (int i = 0; i < array_size; ++i) {
-        sorted[i] = input[i];
-    }
-    for (int i = array_size - 1; i > 0; --i) {
-        for (int j = 0; j < i; ++j) {
-            if (sorted[j] > sorted[j + 1]) {
-                uint64_t tmp = sorted[j];
-                sorted[j] = sorted[j + 1];
-                sorted[j + 1] = tmp;
-            }
-        }
-    }
-
-    // Middle or average of middle values in the sorted input.
-    uint64_t res = 0;
-    if ((array_size % 2) == 0) {
-        res = ((sorted[array_size / 2] / 2) + (sorted[(array_size / 2) - 1]) / 2);
-    } else {
-        res = sorted[array_size / 2];
-    }
-    return res;
-
-}
--- src/util.h
+++ src/util.h
@@ -9,6 +9,4 @@
 
 long map(long x, long in_min, long in_max, long out_min, long out_max);
 
-uint64_t median(uint64_t *input, uint64_t array_size);
-
 #endif /* SC_UTIL_H */
