7d99f47f6e47e435db4e3655554e571ce90928d9
Damave <fdamave@gmail.com>
2019-12-15 01:08:26 +0100

    Add files via upload
    add minimum , maximum speed and if pulse==target_pulse do nothing

--- src/rc.h
+++ src/rc.h
@@ -11,9 +11,9 @@
 #define RC_PWM_OUTER_THRESH 20
 #define RC_PWM_INNER_THRESH 50
 
-
-
-
-
+#define RC_PWM_MINIMUM_FORWARD_SPEED 55    // min 0 .. max 255 (255 is switc
+#define RC_PWM_MOTOR_FORWARD_SPEED_MAX 255 // max 255
+#define RC_PWM_MINIMUM_REVERSE_SPEED 55    // min 0 .. max 255 (255 is switc
+#define RC_PWM_MOTOR_REVERSE_SPEED_MAX 175 // max 255
 
 #endif /* SC_RC_H */
